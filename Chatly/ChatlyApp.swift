09fffb5c7b91ecf44b5b20155634b922db8cbcb9
JG <gawryszewska.joanna@gmail.com>
2024-10-06 17:43:22 +0200

    Initial Commit

---
+++ Chatly/ChatlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatlyApp.swift
+//  Chatly
+//
+//  Created by Joanna Gawryszewska on 2024-10-06.
+//
+
+import SwiftUI
+
+@main
+struct ChatlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
